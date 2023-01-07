3aea5c6a70c6028f94fe67f73467e3e0ab606aae
DLukianchuk <ldvcreative@gmail.com>
2023-01-07 14:51:05 +0200

    Added new parameter: color of the building

--- src/mateAcademy/MethodBuildingHouse.java
+++ src/mateAcademy/MethodBuildingHouse.java
@@ -4,16 +4,16 @@ public class MethodBuildingHouse {
     public static void main(String[] args) {
 
 
-        String littleHouse = buildHouse(5);
-        String bigHouse = buildHouse(9);
+        String littleHouse = buildHouse(5, "green");
+        String bigHouse = buildHouse(9, "red");
 
-        System.out.println(buildHouse(3));
+        System.out.println(buildHouse(3, "yellow"));
         System.out.println(littleHouse);
         System.out.println(bigHouse);
 
     }
-    public static String buildHouse(int numberOfFloors){
-        return numberOfFloors + "-floors building";
+    public static String buildHouse(int numberOfFloors, String color){
+        return color + " " + numberOfFloors + "-floors building";
     }
 
 
