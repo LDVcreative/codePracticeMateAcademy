dcca021e2ed823a8fd299edf950bbaaff7864dae
DLukianchuk <ldvcreative@gmail.com>
2023-01-08 13:15:16 +0200

    Wallet money added

--- src/mateAcademy/BooleanCanBuy.java
+++ src/mateAcademy/BooleanCanBuy.java
@@ -2,25 +2,25 @@ package mateAcademy;
 
 public class BooleanCanBuy {
     public static void main(String[] args) {
-        int userAge = 15;
-        int userBankAccountCash = 50; //The amount of cash in the user's bank account
+        int userAge = 19;
+        int userBankAccountMoney = 8; //The amount of cash in the user's bank account
+        int userWalletCash = 10;
 
         int ageLimit = 18; //Restrictions on the purchase of goods
         int price = 30; //Price of goods
 
-        boolean cashCheck = userBankAccountCash >= price;
         boolean ageCheck = userAge >= ageLimit;
+        boolean userBankMoneyCheck = userBankAccountMoney >= price;
+        boolean userWalletMoneyCheck = userWalletCash >= price;
 
-        if (cashCheck&&ageCheck){
+        if ((userBankMoneyCheck || userWalletMoneyCheck) && ageCheck){
             System.out.println("You can buy this product");
         } else {
             System.out.println("You can't buy this product");
         }
 
         //Another version of checking system:
-        boolean canBuy = cashCheck&&ageCheck;
+        boolean canBuy = (userBankMoneyCheck || userWalletMoneyCheck) && ageCheck;
         System.out.println(canBuy);
-
-
     }
 }
