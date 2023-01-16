688440378a1b48f7b801da53014fe973df3ac0d7
DLukianchuk <ldvcreative@gmail.com>
2023-01-16 22:31:38 +0200

    "For each" added

--- src/mateAcademy/ArrayLesson1.java
+++ src/mateAcademy/ArrayLesson1.java
@@ -3,11 +3,22 @@ package mateAcademy;
 public class ArrayLesson1 {
     public static void main(String[] args) {
         int[] numbers = {10, 20, 30, 40};
+        String[] names = {"John", "Jack", "Jill"};
+        String [] catNames = {"Kitty", "Ditty", "Bitty"};
 
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
 
+        for (int number: numbers){
+            System.out.println(number);
+        }
+        for (String name: names){
+            System.out.println("Your name is... " + name);
+        }
+        for (String catName: catNames){
+            System.out.println("Hello " + catName);
+        }
 
     }
 
