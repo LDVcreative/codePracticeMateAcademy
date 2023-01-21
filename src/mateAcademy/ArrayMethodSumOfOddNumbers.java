2cbf9aadebe1a40d1ec0b2a1cf19b236fe66e4df
DLukianchuk <ldvcreative@gmail.com>
2023-01-21 13:29:35 +0200

    Task and explanations added

--- src/mateAcademy/ArrayMethodSumOfOddNumbers.java
+++ src/mateAcademy/ArrayMethodSumOfOddNumbers.java
@@ -8,11 +8,17 @@ package mateAcademy;
 
 public class ArrayMethodSumOfOddNumbers {
     public static int getSum(int[] numbers) {
+        //Спочатку створюємо перемінну,до якої будуть додаватись всі непарні значення:
         int sumOfOddNumbers = 0;
+        //Потім проітеруємо масив, щоб дістати доступ до кожного значення:
         for (int i = 0; i < numbers.length; i++){
+            //Далі ділитимемо всі значення з масиву numbers[] на 2
+            // і перевіряємо, чи дає воно в остачі 0:
             if (numbers[i] % 2 == 0){
+                //Якщо значення дає 0 в остачі, отже воно парне і тому ми цого пропускаємо:
                 continue;
             }
+            //Значення, що лишились від ітерування почергово додаються до перемінної
             sumOfOddNumbers = sumOfOddNumbers + numbers[i];
         }
         return sumOfOddNumbers;
