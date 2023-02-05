651e173bbc073d0cc66fafd7ca227cb63ceee4e3
DLukianchuk <ldvcreative@gmail.com>
2023-02-05 21:30:38 +0200

    Method to upper case first and last letters in string

---
+++ src/mateAcademy/MarkStringEnds.java
@@ -0,0 +1,17 @@
+package mateAcademy;
+
+public class MarkStringEnds {
+    public static String[] markStringEnds(String originalString) {
+
+        int lastIndex = originalString.length() - 1;
+        String firstLetter = originalString.substring(0, 1).toUpperCase();
+        String lastLetter = originalString.substring(lastIndex).toUpperCase();
+
+        String wordFirstLetterUpCase = firstLetter + originalString.substring(1);
+        String wordLastLetterUpCase = originalString.substring(0, lastIndex) + lastLetter;
+
+
+
+        return new String[]{wordFirstLetterUpCase, wordLastLetterUpCase};
+    }
+}
